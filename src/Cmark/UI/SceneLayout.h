dfed5bd4389e40275de5f430eafff90fc0db6e41
南无仙 <granbao@gmail.com>
2023-12-07 00:14:00 +0800

    update

--- src/Cmark/UI/SceneLayout.h
+++ src/Cmark/UI/SceneLayout.h
@@ -22,22 +22,27 @@ namespace CM
         SceneLayout() = default;
         ~SceneLayout() = default;
 
-        [[nodiscard]] const Margin & getMargin() const {return m_margin;}
+        [[nodiscard]] const Margin & getMargin() const;
         void setMargin(int left,int right,int top,int bottom);
 
         /// logo
         void setLogoSpace(int space);
         [[nodiscard]] int logoSpace() const;
 
-        [[nodiscard]] const CSize & LogoSize()const {return m_LogoSize;}
-
-        void setLogoSize(int w,int h){ setLogoSize({w,h});}
-        void setLogoSize(const CSize & size) {m_LogoSize = size;}
+        [[nodiscard]] const CSize & LogoSize()const;
+        void setLogoSize(int w,int h);
+        void setLogoSize(const CSize & size);
 
     private:
         Margin m_margin;                ///< scene margin{left right top bottom}
         int m_logoSpace {10};           ///< show image space with log
         CSize m_LogoSize{64,64};  ///< logo size
+
+        int leftTextOffset;   ///< 左边部分文字的偏移长度 (left + image.width() + right) * 0.1
+        // logo position      ///< (left + image.width() + right) / 3 * 2的位置的地方
+        // line width 7 pixel
+
+
     };
 
 } // CM
